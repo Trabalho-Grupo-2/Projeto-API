aa9a4ef4ebc546921a6d988dc98ab15fa02e904c
Alfonso Villanueva <alfonso-40200365@esmad.ipp.pt>
2022-05-16 20:37:10 +0100

    fghjk

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-const app = express();
+const app = express()
 const port = process.env.PORT;
 const host = process.env.HOST;
 
